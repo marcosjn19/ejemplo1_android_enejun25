7ee6c097fd7069dd1abb1d06cb5bf11e583ce6f6
marcosjn19 <marcosjn719@gmail.com>
2025-02-11 12:39:52 -0600

    Clase Lunes 10Feb

--- app/src/main/java/com/example/ejemplo1/MainActivity.kt
+++ app/src/main/java/com/example/ejemplo1/MainActivity.kt
@@ -4,13 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.example.ejemplo1.ui.theme.Ejemplo1Theme
@@ -21,31 +20,47 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Ejemplo1Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Chava",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-
-                }
+                GreetingPreview()
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "$name!",
-        fontSize = 50.sp,
-        modifier = modifier,
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    Ejemplo1Theme {
-        Greeting("Chava")
+    Content( "Hola", "Mundo ")
+}
+
+@Composable
+fun Content(mensaje1: String, mensaje2: String) {
+    Column {
+        Text(
+            mensaje1,
+            fontWeight = FontWeight.ExtraBold,
+            fontSize = 28.sp,
+            lineHeight = 28.sp, color = Color.Blue)
+        Text(
+            "Probando Rows",
+            fontWeight = FontWeight.SemiBold,
+            fontSize = 20.sp,
+            lineHeight = 20.sp)
+        Row {
+            Text(mensaje1,
+                fontWeight = FontWeight.SemiBold,
+                fontSize = 30.sp,
+                lineHeight = 20.sp)
+            Column {
+                Text("Columna",
+                    fontWeight = FontWeight.ExtraBold,
+                    fontSize = 10.sp,
+                    lineHeight = 50.sp, color = Color.Red)
+                Text(mensaje2,
+                    fontWeight = FontWeight.ExtraBold,
+                    fontSize = 10.sp,
+                    lineHeight = 5.sp)
+            }
+
+        }
     }
 }
